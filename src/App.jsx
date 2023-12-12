31282ba74424f99da5be6704fca0a8acf3a66e00
Mukam7 <mukamhojaniyazow89@gmail.com>
2023-12-12 13:23:28 +0500

    Finished

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import AuthPage from "./pages/AuthPage/AuthPage";
+import PageLayout from "./Layouts/PageLayout/PageLayout";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase/firebase";
+
+function App() {
+  const [authUser] = useAuthState(auth);
+
+  return (
+    <PageLayout>
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/auth" />}
+        />
+        <Route
+          path="/auth"
+          element={!authUser ? <AuthPage /> : <Navigate to="/" />}
+        />
+        <Route path="/:username" element={<ProfilePage />} />
+      </Routes>
+    </PageLayout>
+  );
+}
+
+export default App;
---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,18 @@
+import { Container, Flex, VStack } from "@chakra-ui/react";
+import AuthForm from "../../components/AuthForm/AuthForm";
+
+const AuthPage = () => {
+  return (
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+      <Container maxW={"container.md"} padding={0}>
+        <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+          <VStack spacing={4} align={"stretch"}>
+            <AuthForm />
+          </VStack>
+        </Flex>
+      </Container>
+    </Flex>
+  );
+};
+
+export default AuthPage;
