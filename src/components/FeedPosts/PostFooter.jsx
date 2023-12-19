9c1ed61d2a39ccbeb7e7d04c5e19e119770470a3
MALIKA <uskenbayevamalika2@gmail.com>
2023-12-19 10:51:55 +0300

    frontend instagram

---
+++ src/components/FeedPosts/PostFooter.jsx
@@ -0,0 +1,94 @@
+import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text, useDisclosure } from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../assets/constants";
+import usePostComment from "../../hooks/usePostComment";
+import useAuthStore from "../../store/authStore";
+import useLikePost from "../../hooks/useLikePost";
+import { timeAgo } from "../../utils/timeAgo";
+import CommentsModal from "../Modals/CommentsModal";
+
+const PostFooter = ({ post, isProfilePage, creatorProfile }) => {
+	const { isCommenting, handlePostComment } = usePostComment();
+	const [comment, setComment] = useState("");
+	const authUser = useAuthStore((state) => state.user);
+	const commentRef = useRef(null);
+	const { handleLikePost, isLiked, likes } = useLikePost(post);
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	const handleSubmitComment = async () => {
+		await handlePostComment(post.id, comment);
+		setComment("");
+	};
+
+	return (
+		<Box mb={10} marginTop={"auto"}>
+			<Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
+				<Box onClick={handleLikePost} cursor={"pointer"} fontSize={18}>
+					{!isLiked ? <NotificationsLogo /> : <UnlikeLogo />}
+				</Box>
+
+				<Box cursor={"pointer"} fontSize={18} onClick={() => commentRef.current.focus()}>
+					<CommentLogo />
+				</Box>
+			</Flex>
+			<Text fontWeight={600} fontSize={"sm"}>
+				{likes} likes
+			</Text>
+
+			{isProfilePage && (
+				<Text fontSize='12' color={"gray"}>
+					Posted {timeAgo(post.createdAt)}
+				</Text>
+			)}
+
+			{!isProfilePage && (
+				<>
+					<Text fontSize='sm' fontWeight={700}>
+						{creatorProfile?.username}{" "}
+						<Text as='span' fontWeight={400}>
+							{post.caption}
+						</Text>
+					</Text>
+					{post.comments.length > 0 && (
+						<Text fontSize='sm' color={"gray"} cursor={"pointer"} onClick={onOpen}>
+							View all {post.comments.length} comments
+						</Text>
+					)}
+					{/* COMMENTS MODAL ONLY IN THE HOME PAGE */}
+					{isOpen ? <CommentsModal isOpen={isOpen} onClose={onClose} post={post} /> : null}
+				</>
+			)}
+
+			{authUser && (
+				<Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
+					<InputGroup>
+						<Input
+							variant={"flushed"}
+							placeholder={"Add a comment..."}
+							fontSize={14}
+							onChange={(e) => setComment(e.target.value)}
+							value={comment}
+							ref={commentRef}
+						/>
+						<InputRightElement>
+							<Button
+								fontSize={14}
+								color={"blue.500"}
+								fontWeight={600}
+								cursor={"pointer"}
+								_hover={{ color: "white" }}
+								bg={"transparent"}
+								onClick={handleSubmitComment}
+								isLoading={isCommenting}
+							>
+								Post
+							</Button>
+						</InputRightElement>
+					</InputGroup>
+				</Flex>
+			)}
+		</Box>
+	);
+};
+
+export default PostFooter;
---
+++ src/components/FeedPosts/PostHeader.jsx
@@ -0,0 +1,51 @@
+import { Avatar, Box, Button, Flex, Skeleton, SkeletonCircle } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import useFollowUser from "../../hooks/useFollowUser";
+import { timeAgo } from "../../utils/timeAgo";
+
+const PostHeader = ({ post, creatorProfile }) => {
+	const { handleFollowUser, isFollowing, isUpdating } = useFollowUser(post.createdBy);
+
+	return (
+		<Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
+			<Flex alignItems={"center"} gap={2}>
+				{creatorProfile ? (
+					<Link to={`/${creatorProfile.username}`}>
+						<Avatar src={creatorProfile.profilePicURL} alt='user profile pic' size={"sm"} />
+					</Link>
+				) : (
+					<SkeletonCircle size='10' />
+				)}
+
+				<Flex fontSize={12} fontWeight={"bold"} gap='2'>
+					{creatorProfile ? (
+						<Link to={`/${creatorProfile.username}`}>{creatorProfile.username}</Link>
+					) : (
+						<Skeleton w={"100px"} h={"10px"} />
+					)}
+
+					<Box color={"gray.500"}>• {timeAgo(post.createdAt)}</Box>
+				</Flex>
+			</Flex>
+			<Box cursor={"pointer"}>
+				<Button
+					size={"xs"}
+					bg={"transparent"}
+					fontSize={12}
+					color={"blue.500"}
+					fontWeight={"bold"}
+					_hover={{
+						color: "white",
+					}}
+					transition={"0.2s ease-in-out"}
+					onClick={handleFollowUser}
+					isLoading={isUpdating}
+				>
+					{isFollowing ? "Unfollow" : "Follow"}
+				</Button>
+			</Box>
+		</Flex>
+	);
+};
+
+export default PostHeader;
