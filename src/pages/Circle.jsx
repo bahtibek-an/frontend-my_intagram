ccc13eb98200a57cd037580cf5427915e6edbda1
Oqilbek <100509683+Oqilbek2121@users.noreply.github.com>
2024-01-07 14:29:23 +0500

    Update Circle.jsx

--- src/pages/Circle.jsx
+++ src/pages/Circle.jsx
@@ -1,14 +1,1 @@
-import React from "react";
-const Circle = () => {
-  return (
-    <>
-      <div className="bg-circles position-fixed">
-        <div className="circle-1"></div>
-        <div className="circle-2"></div>
-        <div className="circle-3"></div>
-      </div>
-    </>
-  );
-};
-
-export default Circle;
+alert("No Cheating");
